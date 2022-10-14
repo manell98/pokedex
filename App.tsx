37d417b86580292a2188b050623b54fb3b858f48
Emanuell Santos de Jesus <dfmanu06@gmail.com>
2022-10-14 16:31:03 -0300

    Mostrando img dos pokemons na tela do celular

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View} from 'react-native';
+import PokemonComponent from "./src/components/PokemonComponent";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <PokemonComponent />
     </View>
   );
 }
@@ -16,5 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
---
+++ src/components/PokemonComponent.tsx
@@ -0,0 +1,51 @@
+import {Image, Text, TouchableOpacity, View} from "react-native";
+import { useState } from "react";
+import axios from "axios";
+import { styles } from "./stylesPokemon";
+
+const PokemonComponent = () => {
+    const [pokemons, setPokemons] = useState([]);
+
+    const [objImgPokemons, setObjImgPokemons] = useState([{}]);
+
+    const listarPokemons = async () => {
+        const result = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=20");
+
+        setPokemons(result.data.results);
+    };
+
+    const listarImgPokemons = () => {
+        const arrayImgs: Array<object> = [];
+
+        pokemons.forEach((pokemon: any, index: number) => {
+            arrayImgs.push({
+                nome: pokemon.name,
+                img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+            });
+        })
+
+        setObjImgPokemons(arrayImgs);
+    };
+
+    return (
+        <View>
+            <TouchableOpacity onPress={listarPokemons}>
+                <Text>Listar Pokemons!</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={listarImgPokemons}>
+                <Text>Listar imgs Pokemons!</Text>
+            </TouchableOpacity>
+
+            <View>
+                {
+                    objImgPokemons.map((objPokemon: any, index: number) => (
+                        <Image style={styles.stretch} key={index} source={{uri: objPokemon.img}} accessibilityLabel={objPokemon.nome} />
+                    ))
+                }
+            </View>
+        </View>
+    );
+}
+
+export default PokemonComponent;
