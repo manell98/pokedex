d490b940b9524b0cce5a4ee285281887d1f96ca7
davissbf <davissbf@gmail.com>
2022-10-15 20:26:59 -0300

    Adicionando busca de pokemojn por campo e tipagens

--- src/components/Pokemon/styles.ts
+++ src/components/Pokemon/styles.ts
@@ -1,29 +1,42 @@
 import {StyleSheet} from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+    div: {
+        marginTop: 42,
+        marginBottom: 42,
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: "center",
+        justifyContent: "center",
+        flexWrap: "wrap"
     },
     img: {
         width: 100,
         height: 100,
     },
-    div: {
-      marginTop: 42,
-      marginBottom: 42,
-      width: '100%',
-      flexDirection: 'row',
-      alignItems: "center",
-      justifyContent: "center",
-      flexWrap: "wrap"
+    viewInput: {
+        width: "100%",
+        marginBottom: 12,
+        alignItems: "center",
+    },
+    input: {
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: "#ddd",
+        paddingLeft: 10,
+        paddingRight: 10,
+    },
+    botao: {
+        marginTop: 20,
+        backgroundColor: "#111",
+        borderRadius: 5,
+        height: 56,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingLeft: 7,
+        paddingRight: 7,
     },
     textoBotao: {
-        alignItems: 'center',
-        justifyContent: 'center',
         color: "#fff",
-        fontSize: 16,
-    },
+    }
 });
