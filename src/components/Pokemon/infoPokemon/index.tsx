fe25e532700f7bb584240611efe164d938b3d9a8
Emanuell Santos de Jesus <dfmanu06@gmail.com>
2022-10-16 17:28:28 -0300

    Criando component CardInfoPokemon

--- src/components/Pokemon/infoPokemon/index.tsx
+++ src/components/Pokemon/infoPokemon/index.tsx
@@ -1,10 +1,9 @@
-import {Image, Text, View} from "react-native";
+import {Text, View} from "react-native";
 import axios from "axios";
 import {useEffect, useState} from "react";
-import {styles, BarraProgressoStats, DivStatsInfo, DivType, DivNomePokemon, DivInfo} from "./styles";
+import {styles, DivNomePokemon} from "./styles";
 
-import {TipoDoPokemonInterface} from "../interfaces/tipoDoPokemon";
-import {EstatisticasDoPokemonInterface} from "../interfaces/estatisticasDoPokemon";
+import CardInfoPokemon from "./cardInfo";
 
 const Index = (props: any) => {
     const obj = props.route.params;
@@ -36,33 +35,10 @@ const Index = (props: any) => {
                 <Text style={styles.texNomePokemon}>{obj.nome[0].toUpperCase() + obj.nome.substring(1)}</Text>
             </DivNomePokemon>
 
-            <DivInfo tipo={infoPokemon.types[0].type.name}>
-                <View style={styles.divInfoHeader}>
-                    <Image style={styles.img} source={{uri: obj.urlImg}} accessibilityLabel={obj.nome} />
-
-                    {
-                        infoPokemon.types.map((objType: TipoDoPokemonInterface) => (
-                            <DivType key={objType.slot} tipo={objType.type.name}>
-                                <Text style={styles.textType}>{objType.type.name.toUpperCase()}</Text>
-                            </DivType>
-                        ))
-                    }
-                </View>
-
-                <View style={styles.divStats}>
-                    {
-                        infoPokemon.stats.map((objStats: EstatisticasDoPokemonInterface) => (
-                            <DivStatsInfo key={objStats.stat.name} valor={objStats.base_stat}>
-                                <Text style={styles.textStatName}>{objStats.stat.name.toUpperCase()}</Text>
-
-                                <BarraProgressoStats valor={objStats.base_stat}>
-                                    <Text style={styles.textStatBaseStat}>{objStats.base_stat}</Text>
-                                </BarraProgressoStats>
-                            </DivStatsInfo>
-                        ))
-                    }
-                </View>
-            </DivInfo>
+            <CardInfoPokemon
+                infoPokemon={infoPokemon}
+                obj={obj}
+            />
         </View>
     );
 }
--- src/components/Pokemon/infoPokemon/styles.ts
+++ src/components/Pokemon/infoPokemon/styles.ts
@@ -1,33 +1,7 @@
 import {StyleSheet} from "react-native";
 import styled from 'styled-components/native';
 import { definirCorPorTipoPokemon } from "../utils/utils";
-
-const definirCorBarraDeProgresso = (valor: number) => {
-    let cor = '#8B0000';
-
-    if (valor >= 60 && valor < 80) cor = '#FF8C00';
-
-    if (valor >= 80) cor = '#006400';
-
-    return cor;
-}
-
-export type BarraProgressoType = {
-    valor: number;
-}
-
-export type TipoPokemon = {
-    tipo: string;
-}
-
-export const DivInfo = styled.View<TipoPokemon>`
-  background-color: ${ ({ tipo }) => {
-    // @ts-ignore
-    const objCor = definirCorPorTipoPokemon[tipo] || '#ccc';
-
-    return objCor.corComOpacidade;
-} };
-`;
+import {TipoPokemon} from "./cardInfo/styles";
 
 export const DivNomePokemon = styled.View<TipoPokemon>`
   background-color: ${ ({ tipo }) => {
@@ -40,43 +14,6 @@ export const DivNomePokemon = styled.View<TipoPokemon>`
   justify-content: center;
 `;
 
-export const DivType = styled.View<TipoPokemon>`
-  margin-left: 20px;
-  background-color: ${ ({ tipo }) => {
-    // @ts-ignore
-    const objCor = definirCorPorTipoPokemon[tipo] || '#ccc';
-
-    return objCor.cor;
-  } };
-  height: 40px;
-  width: 80px;
-  align-items: center;
-  justify-content: center;
-  border-radius: 25px;
-`;
-
-export const DivStatsInfo = styled.View<BarraProgressoType>`
-  flex-direction: row-reverse;
-  align-items: center;
-  justify-content: space-between;
-  margin: 2px;
-  border-style: solid;
-  border-width: 2px;
-  border-radius: 25px;
-  border-color: ${ ({ valor }) => definirCorBarraDeProgresso(valor) };
-`;
-
-export const BarraProgressoStats = styled.View<BarraProgressoType>`
-  height: 100%;
-  width: ${ ({ valor }) => valor >= 30 ? valor : 30 }px;
-  background-color: ${ ({ valor }) => definirCorBarraDeProgresso(valor) };
-  border-style: solid;
-  border-radius: 15px;
-  border-color: transparent;
-  align-items: flex-start;
-  padding: 5px;
-`;
-
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -84,37 +21,10 @@ export const styles = StyleSheet.create({
         paddingBottom: 42,
         backgroundColor: '#131016',
     },
-    img: {
-        width: 100,
-        height: 100,
-    },
     texNomePokemon: {
         color: '#fff',
         fontSize: 25,
         margin: 10,
         fontWeight: "bold"
-    },
-    divInfoHeader: {
-        marginLeft: 20,
-        width: '100%',
-        flexDirection: 'row',
-        flexWrap: "wrap",
-        alignItems: "center",
-    },
-    textType: {
-        color: '#fff',
-        fontWeight: "bold"
-    },
-    divStats: {
-        margin: 20,
-    },
-    textStatName: {
-        color: '#fff',
-        fontWeight: "bold",
-        paddingRight: 10,
-    },
-    textStatBaseStat: {
-        color: '#fff',
-        fontWeight: "bold",
-    },
+    }
 });
